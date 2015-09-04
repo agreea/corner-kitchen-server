67f48e37ceafcd44a7c551b33a9b810ef310bab6
Agree Ahmed <agreea@hotmail.com>
2015-09-04 00:48:30 -0400

    kitchenuser output

--- session.go
+++ session.go
@@ -82,7 +82,7 @@ func (t *SessionManager) CreateSessionForGuest(uid int64) (string, error) {
 	// }
 	// Create the session object and put it in the local cache
 	GuestSession := new(KitchenSession)
-	GuestSession.Guest // = guest_data
+	// GuestSession.Guest // = guest_data
 	GuestSession.Expires = time.Now().Add(60 * 24 * time.Hour)
 
 	// Store the token in the database
