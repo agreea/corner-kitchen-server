d7473876ffaac560ee4399c920725b13d43ebb5d
Agree Ahmed <agreea@hotmail.com>
2015-09-04 00:57:08 -0400

    kitchenuser output

--- session.go
+++ session.go
@@ -76,14 +76,14 @@ func (t *SessionManager) CreateSessionForGuest(uid int64) (string, error) {
 	// 	return guest_session, err
 	// }
 	// Get the user's info
-	guest_data, err := GetGuestById(t.db, uid)
-	if err != nil {
-		return "getting guest by id", err
-	}
-	Create the session object and put it in the local cache
-	GuestSession := new(KitchenSession)
-	GuestSession.Guest = guest_data
-	GuestSession.Expires = time.Now().Add(60 * 24 * time.Hour)
+	// guest_data, err := GetGuestById(t.db, uid)
+	// if err != nil {
+	// 	return "getting guest by id", err
+	// }
+	// Create the session object and put it in the local cache
+	// GuestSession := new(KitchenSession)
+	// GuestSession.Guest = guest_data
+	// GuestSession.Expires = time.Now().Add(60 * 24 * time.Hour)
 
 	// Store the token in the database
 	// _, err = t.db.Exec(`INSERT INTO  GuestSession (
