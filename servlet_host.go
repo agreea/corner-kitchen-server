147060c4fb98fa9050a7251c92abdae3d8db93cf
Agree Ahmed <agreea@hotmail.com>
2015-09-18 03:03:40 -0400

    Got rid of get host and guest

--- servlet_host.go
+++ servlet_host.go
@@ -42,23 +42,34 @@ func NewHostServlet(server_config *Config, session_manager *SessionManager, twil
 /*
 https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_6n8She3UUNpFgbv1sYtB28b6Db7sTLY6&scope=read_write
 curl --data "method=StripeConnect&session=c12c1704-d2b0-4af5-83eb-a562afcfe277&auth=ac_6ygyqZ4QBFVNl5s7z7VgEVULAMFaNoT7"  https://yaychakula.com/api/host
+
+curl https://api.stripe.com/v1/charges \
+   -u ***REMOVED***: \
+   -d amount=100 \
+   -d currency=usd \
+   -d customer=cus_6yTtZHwEUbDA4v \
+   -d destination=acct_16l61lJgURr5uCR5 \
+   -d application_fee=20
+
 */
 func (t *HostServlet) StripeConnect(r *http.Request) *ApiResult {
 	log.Println("=======Stripe Connect called======")
 	auth := r.Form.Get("auth")
 	session_id := r.Form.Get("session")
-	guest, host, err := t.get_guest_and_host(session_id)
+	valid, session, err := t.session_manager.GetGuestSession(session_id)
 	if err != nil {
 		log.Println(err)
-		return APIError("Invalid session", 500)
+		return APIError(err, 400)
 	}
-	if guest == nil { // you had an actual session but it was old
-		log.Println("Expired session")
-		return APIError("Expired session", 500)
+	if !valid {
+		return APIError("Invalid session", 400)
 	}
-	if host == nil {
-		log.Println("Couldn't find host")
-		return APIError("Could not locate host", 400)
+	guest := session.Guest
+	host, err := GetHostByGuestId(t.db, guest.Id)
+	// create a host if there isn't one and then update their data
+	if err != nil {
+		log.Println(err)
+		return APIError(err, 400)
 	}
 	stripeResponse, err := t.stripe_auth(auth)
 	if err != nil {
@@ -70,6 +81,7 @@ func (t *HostServlet) StripeConnect(r *http.Request) *ApiResult {
 		return APIError(stripe_error.(string)+stripeResponse["error_description"].(string), 400)
 	}
 	if stripeResponse["livemode"].(bool) == false {
+		lot.Println("Stripe wasn't in live mode")
 		return APIError("Stripe misconfiguration.", 500)
 	}
 	err = UpdateStripeConnect(t.db, stripeResponse, host.Id)
@@ -77,22 +89,21 @@ func (t *HostServlet) StripeConnect(r *http.Request) *ApiResult {
 		log.Println(err)
 		return APIError("Could not update Stripe Data. Please try again.", 500)
 	}
-	log.Println(stripeResponse["stripe_user_id"].(string))
 	log.Println(stripeResponse)
 	return APISuccess(nil)
 }
 
 func (t *HostServlet) UpdateHost(r *http.Request) *ApiResult {
 	session_id := r.Form.Get("session")
-	guest, host, err := t.get_guest_and_host(session_id)
+	valid, session, err := t.session_manager.GetGuestSession(session_id)
 	if err != nil {
 		log.Println(err)
-		return APIError("Invalid session", 500)
+		return APIError(err, 400)
 	}
-	if guest == nil { // you had an actual session but it was old
-		log.Println("Expired session")
-		return APIError("Expired session", 500)
+	if !valid {
+		return APIError("Invalid session", 400)
 	}
+	guest := session.Guest
 	// Update the guest data according to the form
 	first_name := r.Form.Get("firstName")
 	last_name := r.Form.Get("lastName")
@@ -103,15 +114,15 @@ func (t *HostServlet) UpdateHost(r *http.Request) *ApiResult {
 		log.Println(err)
 		return APIError("Failed to update host data", 500)
 	}
+	host, err := GetHostByGuestId(t.db, guest.Id)
 	// create a host if there isn't one and then update their data
-	if host == nil {
-		// create host
+	if err != nil {
 		err = CreateHost(t.db, guest.Id)
 		if err != nil {
 			return APIError("Failed to create host", 500)
 		}
 		host, err := GetHostByGuestId(t.db, guest.Id)
-		if err != nil && host == nil {
+		if err != nil {
 			return APIError("Failed to locate host", 500)
 		}
 	}
@@ -147,30 +158,34 @@ func (t *HostServlet) stripe_auth(auth string) (map[string]interface{}, error) {
 		log.Println(err)
 		return nil, err
 	}
-
 	return stripeJSON, nil
 }
 
 func (t *HostServlet) GetHost(r *http.Request) *ApiResult {
 	session_id := r.Form.Get("session")
-	guest, host, err := t.get_guest_and_host(session_id)
+	valid, session, err := t.session_manager.GetGuestSession(session_id)
 	if err != nil {
 		log.Println(err)
-		return APIError("Invalid session", 500)
+		return APIError(err, 400)
 	}
-	if guest == nil { // you had an actual session but it was old
-		log.Println("Expired session")
-		return APIError("Expired session", 500)
+	if !valid {
+		return APIError("Invalid session", 400)
 	}
+	guest := session.Guest
 	host_resp := new(HostResponse)
 	host_resp.First_name = guest.First_name
 	host_resp.Last_name = guest.Last_name
 	host_resp.Email = guest.Email
 	host_resp.Phone = guest.Phone
 	host_resp.Prof_pic = GetFacebookPic(guest.Facebook_id)
-	if host == nil { 
-		// host hasn't been created yet.
-		// Give em what we got on the guest and then create a host object when they updateHost
+	host, err := GetHostByGuestId(t.db, host_as_guest.Id)
+	if err != nil { // there wasn't a host with this guest id
+		log.Println(err)
+		err = CreateHost(t.db, guest.Id)
+		if err != nil {
+			log.Println(err)
+			return APIError(err, 500)
+		}
 		host_resp.Stripe_connect = false
 		return APISuccess(host_resp)
 	}
@@ -179,21 +194,6 @@ func (t *HostServlet) GetHost(r *http.Request) *ApiResult {
 	return APISuccess(host_resp)
 }
 
-func (t *HostServlet) get_guest_and_host(session_id string) (*GuestData, *HostData, error) {
-	valid, session, err := t.session_manager.GetGuestSession(session_id)
-	if err != nil {
-		return nil, nil, err
-	}
-	if !valid {
-		return nil, nil, nil
-	}
-	host_as_guest := session.Guest
-	host, err := GetHostByGuestId(t.db, host_as_guest.Id)
-	if err != nil {
-		return host_as_guest, nil, err
-	}
-	return host_as_guest, host, nil
-}
 // func (t *HostServlet) Pay(r *http.Request) *ApiResult {
 // get the guest's Stripe id
 // get the meal data
