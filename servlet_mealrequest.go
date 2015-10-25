ed6b4f4455a8ba162f1afc8460b028cb06d9f1f1
Agree Ahmed <agreea@hotmail.com>
2015-10-25 16:18:25 -0400

    testing prod vs qa goroutines

--- servlet_mealrequest.go
+++ servlet_mealrequest.go
@@ -219,7 +219,7 @@ func (t *MealRequestServlet) email_guest(guest *GuestData, host *HostData, meal 
 		html :=fmt.Sprintf("<p>Get excited!</p><p>The dinner is at %s, %s</p>" + 
 							"<p>Please reply to this email if you need any help.</p>" +
 							"<p>View the meal again <a href=https://yaychakula.com/meal.html?Id=" + meal.Id + ">here</a> " +
-							"<p>Peace, love and full stomachs,</p>"
+							"<p>Peace, love and full stomachs,</p>" +
 							"<p>Chakula</p>", host.Address, BuildTime(meal.Starts))
 		SendEmail(guest.Email, subject, html)
 	} else {
