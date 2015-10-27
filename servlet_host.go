ab370782c1c44d9b9322f541916ab2739fc89f26
Agree Ahmed <agreea@hotmail.com>
2015-10-27 16:37:43 -0400

    testing ish

--- servlet_host.go
+++ servlet_host.go
@@ -104,7 +104,7 @@ func (t *HostServlet) Get(r *http.Request) *ApiResult {
 		return APIError("Invalid session", 400)
 	}
 
-	host, err := GetHostByGuestId(t.db, session.Guest_id)
+	host, err := GetHostByGuestId(t.db, session.Guest.Id)
 	if err != nil {
 		log.Println(err)
 		return APIError("No host in db matching this record", 400)
