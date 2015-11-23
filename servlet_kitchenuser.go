99e1fdb94e08c98168e895fecfe6ecfd9fa8585f
Agree Ahmed <agreea@hotmail.com>
2015-11-23 15:49:12 -0500

    frontend testing

--- servlet_kitchenuser.go
+++ servlet_kitchenuser.go
@@ -236,7 +236,7 @@ func (t *KitchenUserServlet) get_fb_long_token(fb_token string) (long_token stri
 	}
 }
 /*
-curl --data "method=Get&session=f1caa66a-3351-48db-bcb3-d76bdc644634" https://qa.yaychakula.com/api/kitchenuser
+curl --data "method=Get&session=08534f5c-04cd-4d37-9675-b0dc71c0ddaf" https://qa.yaychakula.com/api/kitchenuser
 */
 func (t *KitchenUserServlet) Get(r *http.Request) *ApiResult {
 	session_id := r.Form.Get("session")
