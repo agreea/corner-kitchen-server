868692403118b95b27f2f976531f31d264dee276
Agree <agree.ahmed@gmail.com>
2016-03-19 13:23:31 -0400

    new gif servlet

---
+++ servlet_gif.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"net/http"
+)
+
+type GifServlet struct {
+	db              *sql.DB
+	server_config   *Config
+}
+
+func NewGifServlet(server_config *Config) *GifServlet {
+	t := new(GifServlet)
+
+	t.server_config = server_config
+
+	db, err := sql.Open("mysql", server_config.GetSqlURI())
+	if err != nil {
+		log.Fatal("NewTruckServlet", "Failed to open database:", err)
+	}
+	t.db = db
+	return t
+}
+
+func (t *GifServlet) Upload(r *http.Request) *ApiResult {
+	// get gif
+	// write to file
+	// get your permissions right
+	// return the URL
+	gif_str := r.Form.Get("gif")
+	file_name, err := CreatePicFile(gif_str)
+	if err != nil {
+		log.Println(err)
+	}
+	return APISuccess("https://yaychakula.com/img/" + file_name)
+}
